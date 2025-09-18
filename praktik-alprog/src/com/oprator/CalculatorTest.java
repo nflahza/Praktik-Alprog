61387ec00d8d576a0c9c7882724f688da0223740
NaFl <72364452+nflahza@users.noreply.github.com>
2025-09-18 13:46:43 +0700

    Modul 1

---
+++ praktik-alprog/src/com/oprator/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.oprator;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 7;
+        System.out.println("Hasil Penjumlahan: " + (a+b));
+        System.out.println("Hasil Pengurangan: " + (a-b));
+        System.out.println("Hasil Perkalian: " + (a*b));
+        System.out.println("Hasil Pembagian: " + (a/b));
+        System.out.println("Sisa: " + (a%b));
+    }
+}
